0ccf93a052802a675537261737f360706b0e44c8
David Shin <davidshin@wustl.edu>
2015-10-07 15:33:46 -0500

    Hash Table
    Hash Table for DNA sequence reading implemented in algorithm class

---
+++ Hash Table/Record.java
@@ -0,0 +1,28 @@
+//
+// RECORD.JAVA
+// Record type for string hash table
+//
+// A record associates a certain string (the key value) with 
+// a list of sequence positions at which that string occurs.
+//
+
+import java.util.*;
+
+
+
+
+public class Record {
+    public String key;
+    public ArrayList<Integer> positions;
+    public int intHashKey;
+    public int pos;
+    
+    public Record(String s)
+    {
+	key = s;
+	intHashKey = StringTable.toHashKey(s);
+	positions = new ArrayList<Integer>(1);
+			
+			
+    }
+}
