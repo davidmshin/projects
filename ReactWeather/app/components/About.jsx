c66935208c4993aac98b855be72e77ce6a96e39a
David Shin <davidshin@wustl.edu>
2016-11-27 13:18:11 -0600

    Add styles

--- ReactWeather/app/components/About.jsx
+++ ReactWeather/app/components/About.jsx
@@ -1,13 +1,31 @@
 var React = require('react');
 
+var About = React.createClass({
+  render: function() {
+    var content = {
+      flex: 0.8
+    };
 
-var About = (props) => {
-  return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to About Page!</p>
-    </div>
-  )
-};
+    return (
+      <div style={content}>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application built on React. Wanted to learn React so this was something
+        helped me familiarized with the framework</p>
+        <p>
+          Here are some tools I used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a>JavaScript framework React
+          </li>
+          <li>
+            <a href="https://openweathermap.org">Open Weather Map</a>Thanks to the API provided
+              by Open Weather Map, I could retrieve weather data for different city in real time.
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
 
 module.exports = About;
